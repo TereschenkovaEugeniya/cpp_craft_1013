75bc13e713edc878bdec385a1872e2cabc55558b
fobos <fobostm@gmail.com>
2013-12-02 01:17:02 +0300

    started 6

---
+++ solutions/mike_tolmachev/6/sources/market_data_receiver/market_data_receiver.cpp
@@ -0,0 +1,85 @@
+#include "market_data_receiver.h"
+
+namespace multicast_communication
+{
+
+const size_t market_data_receiver::default_buffer_size = 16ul;
+
+market_data_receiver::market_data_receiver( boost::asio::io_service& io_service, const std::string& multicast_address, unsigned short port )
+	: io_service_( io_service )
+	, listen_endpoint_( boost::asio::ip::address::from_string( "0.0.0.0" ), port )
+	, socket_( io_service_ )
+	, multicast_address_( multicast_address )
+{
+	socket_reload_();
+	register_listen_();
+}
+market_data_receiver::~market_data_receiver()
+{
+}
+const std::vector< std::string > market_data_receiver::messages() const
+{
+	boost::mutex::scoped_lock lock( protect_messages_ );
+	return messages_;
+}
+
+void market_data_receiver::socket_reload_()
+{
+	using boost::asio::ip::udp;
+	using boost::asio::ip::address;
+	using boost::asio::ip::multicast::join_group;
+
+	socket_.open( listen_endpoint_.protocol() );
+	socket_.set_option( udp::socket::reuse_address( true ) );
+	socket_.bind( listen_endpoint_ );
+	socket_.set_option( join_group( address::from_string( multicast_address_ ) ) );
+}
+void market_data_receiver::register_listen_( buffer_type pre_buffer, const size_t previous_size )
+{
+	buffer_type buffer;
+	
+	if( pre_buffer )
+		buffer = pre_buffer;
+	else
+		buffer.reset( new std::string( default_buffer_size, '\0' ) );
+
+	char* const buffer_start = &(*buffer->begin());
+
+	using namespace boost::asio::placeholders;
+	socket_.async_receive( boost::asio::buffer( buffer_start, previous_size + default_buffer_size ), 
+		boost::bind( &udp_listener::listen_handler_, this, buffer, error, bytes_transferred ) );
+}
+
+void market_data_receiver::listen_handler_( buffer_type bt, const boost::system::error_code& error, const size_t bytes_received )
+{
+	if ( error )
+	{
+		static const int NO_ENOUGHT_BUFFER = 234;
+		if ( error.value() == NO_ENOUGHT_BUFFER )
+		{
+			enlarge_buffer_( bt );
+			register_listen_( bt, bytes_received );
+		}
+		return;
+	}
+	if ( bytes_received == bt->size() && (*bt)[ bytes_received - 1 ] != '\0' )
+	{
+		enlarge_buffer_( bt );
+		register_listen_( bt, bytes_received );
+	}
+	else
+	{
+		{
+			boost::mutex::scoped_lock lock( protect_messages_ );
+			messages_.push_back( std::string( bt->c_str(), bytes_received ) );
+		}
+		register_listen_();
+	}
+}
+
+void market_data_receiver::enlarge_buffer_( buffer_type& bt )
+{
+	bt->resize( bt->size() + default_buffer_size );
+}
+
+} // namespace multicast_communication
